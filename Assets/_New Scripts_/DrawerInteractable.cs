8d3315f77877e2df5f989dd37c65a9424cf92b57
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-02-27 01:15:27 -0500

    Implement limit checks for Drawer Movement function interactable Change Drawer asset to only track movement on the Z plane

--- Assets/_New Scripts_/DrawerInteractable.cs
+++ Assets/_New Scripts_/DrawerInteractable.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class DrawerInteractable : XRGrabInteractable
 {
+    [SerializeField] Transform drawerTransform;
     [SerializeField] XRSocketInteractor keySocket;
     [SerializeField] bool isLocked;
     private Transform parentTransform;
     private const string defaultLayer = "Default";
     private const string grabLayer = "Grab";
+    private bool isGrabbed;
+    private Vector3 limitPositions;
+    [SerializeField] private Vector3 limitDistances = new Vector3(.02f,.02f,0);
     void Start()
     {   
         if(keySocket != null)
@@ -18,41 +22,67 @@ public class DrawerInteractable : XRGrabInteractable
             keySocket.selectExited.AddListener(OnDrawerLocked);
         }
         parentTransform = transform.parent.transform;
+        limitPositions = drawerTransform.localPosition;
     }
     private void OnDrawerLocked(SelectExitEventArgs arg0)
     {
         isLocked = true;
         Debug.Log("****DRAWER LOCKED");
     }
-
     private void OnDrawerUnlocked(SelectEnterEventArgs arg0)
     {
         isLocked = false;
         Debug.Log("****DRAWER UNLOCKED");
     }
-
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         if(!isLocked)
         {
-           transform.SetParent(parentTransform);                     
+           transform.SetParent(parentTransform);
+           isGrabbed = true;                     
         }
         else
         {
-            interactionLayers = InteractionLayerMask.GetMask(defaultLayer);
+            ChangeLayerMask(defaultLayer);
         }
     }
-
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
-        base.OnSelectExited(args);
-        interactionLayers = InteractionLayerMask.GetMask(grabLayer);
+        base.OnSelectExited(args);        
+        ChangeLayerMask(grabLayer);   
+        isGrabbed = false; 
+        transform.localPosition = drawerTransform.localPosition;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(isGrabbed && drawerTransform != null)
+        {
+            drawerTransform.localPosition = new Vector3(drawerTransform.localPosition.x
+            , drawerTransform.localPosition.y, transform.localPosition.z);
+
+            CheckLimits();
+        }
+    }
+
+    private void CheckLimits()
+    {
+        if(transform.localPosition.x >= limitPositions.x + limitDistances.x ||
+            transform.localPosition.x <= limitPositions.x - limitDistances.x)
+        {
+            ChangeLayerMask(defaultLayer);
+        }
+        else if(transform.localPosition.y >= limitPositions.y + limitDistances.y ||
+            transform.localPosition.y <= limitPositions.y - limitDistances.y)
+        {
+            ChangeLayerMask(defaultLayer);
+        }
+    }
+
+    private void ChangeLayerMask(string mask)
+    {
+        interactionLayers = InteractionLayerMask.GetMask(mask);
     }
 }
 
