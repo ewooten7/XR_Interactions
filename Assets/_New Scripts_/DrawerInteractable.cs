cf854cbe50d2a05fb0ff742d18727397757afbbe
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-02-27 00:41:47 -0500

    Use Inheritance to extend functionality of XR Grab Interactable Class. Improving Drawer Interactable Script: Drawer Interactable Script overrides Grab Interactable Script for both “Is Locked” and “On Select Entered”, so that it retains transformed parent.

--- Assets/_New Scripts_/DrawerInteractable.cs
+++ Assets/_New Scripts_/DrawerInteractable.cs
@@ -3,17 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-public class DrawerInteractable : MonoBehaviour
+public class DrawerInteractable : XRGrabInteractable
 {
     [SerializeField] XRSocketInteractor keySocket;
     [SerializeField] bool isLocked;
+    private Transform parentTransform;
+    private const string defaultLayer = "Default";
+    private const string grabLayer = "Grab";
     void Start()
-    {
+    {   
         if(keySocket != null)
         {
             keySocket.selectEntered.AddListener(OnDrawerUnlocked);
             keySocket.selectExited.AddListener(OnDrawerLocked);
         }
+        parentTransform = transform.parent.transform;
     }
     private void OnDrawerLocked(SelectExitEventArgs arg0)
     {
@@ -27,9 +31,28 @@ public class DrawerInteractable : MonoBehaviour
         Debug.Log("****DRAWER UNLOCKED");
     }
 
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        if(!isLocked)
+        {
+           transform.SetParent(parentTransform);                     
+        }
+        else
+        {
+            interactionLayers = InteractionLayerMask.GetMask(defaultLayer);
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        interactionLayers = InteractionLayerMask.GetMask(grabLayer);
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 }
+
