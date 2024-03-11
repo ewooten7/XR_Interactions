a96d1c7dae2886c89866920985ef8681bb6e05ea
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-03-11 00:03:58 -0400

    Add Event Listeners to Start Button Add events to SimpleUIControl.cs file Change message Text and Activate the Key indicator Light after start button is pressed

--- Assets/_New Scripts_/SimpleUIControl.cs
+++ Assets/_New Scripts_/SimpleUIControl.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+using System;
 
 public class SimpleUIControl : MonoBehaviour
 {
+    [SerializeField] XRButtonInteractable startButton;
+    [SerializeField] string[] msgStrings;
+    [SerializeField] TMP_Text[] msgTexts;
+
+    [SerializeField] GameObject keyIndicatorLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(startButton != null)
+        {
+            startButton.selectEntered.AddListener(StartButtonPressed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartButtonPressed(SelectEnterEventArgs arg0)
     {
-        
+        SetText(msgStrings[1]); 
+        if(keyIndicatorLight != null)
+        {
+            keyIndicatorLight.SetActive(true);
+        }
+    }
+
+    public void SetText(string msg)
+    {
+        for (int i = 0; i < msgTexts.Length; i++)
+        {
+            msgTexts[i].text = msg;
+        }
     }
 }
