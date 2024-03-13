1b4c02aaa206fdde023820ea73a094fb2902907f
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-03-12 22:13:05 -0400

    initial edits setting up

--- Assets/_New Scripts_/CombinationLock.cs
+++ Assets/_New Scripts_/CombinationLock.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CombinationLock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
     void Start()
     {
         
