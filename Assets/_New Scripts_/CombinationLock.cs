9221a9d785ff6789f9bf93842970ce955f9eb3c2
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-03-14 00:45:39 -0400

    Change Combo lock to give unlocked icon if correct combination input

--- Assets/_New Scripts_/CombinationLock.cs
+++ Assets/_New Scripts_/CombinationLock.cs
@@ -1,52 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
+using UnityEngine.UI;
 
 public class CombinationLock : MonoBehaviour
 {
     [SerializeField] TMP_Text userInputText;
     [SerializeField] XRButtonInteractable[] comboButtons;
+    [SerializeField] Image lockedPanel;
+    [SerializeField] Color unlockedColor;
+    [SerializeField] TMP_Text lockedText;
+    private const string unlockedString = "unlocked";
     [SerializeField] bool isLocked;
-    [SerializeField] int[] comboValues = new int [3];
+    [SerializeField] int[] comboValues = new int[3];
     [SerializeField] int[] inputValues;
     private int maxButtonPresses;
     private int buttonPresses;
-    
     void Start()
     {
         maxButtonPresses = comboValues.Length;
         inputValues = new int[comboValues.Length];
         userInputText.text = "";
-        for (int i=0; i< comboButtons.Length; i++)
+        for (int i = 0; i < maxButtonPresses; i++)
         {
             comboButtons[i].selectEntered.AddListener(OnComboButtonPressed);
         }
-        
     }
-
     private void OnComboButtonPressed(SelectEnterEventArgs arg0)
     {
-        if(buttonPresses >= maxButtonPresses) //TOO MANY BUTTON PRESS CHECK
+        if (buttonPresses >= maxButtonPresses)
         {
-
+            //TOO MANY BUTTON PRESSES
         }
         else
         {
-        for (int i = 0; i < comboButtons.Length; i++)
-        {
-            if(arg0.interactableObject.transform.name == comboButtons[i].transform.name)
+            for (int i = 0; i < maxButtonPresses; i++)
             {
-                userInputText.text = i.ToString();
+                if (arg0.interactableObject.transform.name == comboButtons[i].transform.name)
+                {
+                    userInputText.text += i.ToString();
+                    inputValues[buttonPresses] = i;
+                }
+                else
+                {
+                    comboButtons[i].ResetColor();
+                }
             }
-            else
+            buttonPresses++;
+            if (buttonPresses == maxButtonPresses)
             {
-                comboButtons[1].ResetColor();
+                //CHECK COMBO
+                CheckCombo();
             }
-        }
-        buttonPresses++;
         }
     }
+    private void CheckCombo()
+    {
+        int matches = 0;
 
+        for (int i = 0; i < maxButtonPresses; i++)
+        {
+            if (inputValues[i] == comboValues[i])
+            {
+                matches++;
+            }
+        }
+        if (matches == maxButtonPresses)
+        {
+            isLocked = false;
+            lockedPanel.color = unlockedColor;
+            lockedText.text = unlockedString;
+        }
+        else
+        {
+            ResetUserValues();
+        }
+    }
+    private void ResetUserValues()
+    {
+        inputValues = new int[maxButtonPresses];
+        userInputText.text = "";
+        buttonPresses = 0;
+    }
 }
+
