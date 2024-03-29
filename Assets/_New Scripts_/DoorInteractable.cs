03a0c0fe0439082cb91157e76d15dae84dfef697
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-03-29 00:15:14 -0400

    Create Door interaction script Set up door hinges to interact with cabinet

--- Assets/_New Scripts_/DoorInteractable.cs
+++ Assets/_New Scripts_/DoorInteractable.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorInteractable : MonoBehaviour
+public class DoorInteractable : SimpleHingeInteractable
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform doorObject;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        if(doorObject != null)
+        {
+            doorObject.localEulerAngles = new Vector3(
+                doorObject.localEulerAngles.x,
+                transform.localEulerAngles.y,
+                doorObject.localEulerAngles.z
+            );
+        }
     }
 }
