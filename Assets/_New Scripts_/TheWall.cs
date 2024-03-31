2078bdc331013ac9c520d09280733f13c2dffe28
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-03-31 01:44:21 -0400

    Spawn Wall Cubes using prefabs instead of models that remain in game Modify Wall Script to add variables set above 1st wall cube position so that cubes would generate on top of one another

--- Assets/_New Scripts_/TheWall.cs
+++ Assets/_New Scripts_/TheWall.cs
@@ -6,17 +6,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TheWall : MonoBehaviour
 {
+    [SerializeField] GameObject wallCubePrefab;
+    [SerializeField] GameObject socketWallPrefab;
     [SerializeField] XRSocketInteractor wallSocket;
     [SerializeField] GameObject[] wallCubes;
+    [SerializeField] float cubeSpacing = 0.005f;
+    private Vector3 cubeSize;
+    private Vector3 spawnPosition;
     void Start()
     {
-        if (wallSocket != null)
+        if (wallCubePrefab != null)
         {
-            wallSocket.selectEntered.AddListener(OnSocketEnter);
-            wallSocket.selectExited.AddListener(OnSocketExited);
+            cubeSize = wallCubePrefab.GetComponent<Renderer>().bounds.size;
+        }
+
+        spawnPosition = transform.position;
+        BuildWall();
+    }
+    private void BuildWall()
+    {
+        wallCubes = new GameObject[2];
+        if (wallCubePrefab != null)
+        {
+            wallCubes[0] = Instantiate(wallCubePrefab, spawnPosition, transform.rotation);
+        }
+        spawnPosition.y += cubeSize.y + cubeSpacing;
+        
+        if (socketWallPrefab != null)
+        {
+            wallCubes[1] = Instantiate(socketWallPrefab, spawnPosition, transform.rotation);
+            wallSocket = wallCubes[1].GetComponentInChildren<XRSocketInteractor>();
+
+            if (wallSocket != null)
+            {
+                wallSocket.selectEntered.AddListener(OnSocketEnter);
+                wallSocket.selectExited.AddListener(OnSocketExited);
+            }
+        }
+        for (int i = 0; i < wallCubes.Length; i++)
+        {
+            if (wallCubes[i] != null)
+            {
+                wallCubes[i].transform.SetParent(transform);
+            }
         }
     }
-
     private void OnSocketExited(SelectExitEventArgs arg0)
     {
         for (int i = 0; i < wallCubes.Length; i++)
@@ -28,7 +62,6 @@ public class TheWall : MonoBehaviour
             }
         }
     }
-
     private void OnSocketEnter(SelectEnterEventArgs arg0)
     {
         for (int i = 0; i < wallCubes.Length; i++)
@@ -47,3 +80,4 @@ public class TheWall : MonoBehaviour
 
     }
 }
+
