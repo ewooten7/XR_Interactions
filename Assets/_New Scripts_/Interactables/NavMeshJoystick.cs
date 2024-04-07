6135eb3f0791de16008ac2ada27b409b17b4ecb6
ewooten7 <111931887+ewooten7@users.noreply.github.com>
2024-04-07 02:38:33 -0400

    Create Joystick remote connection to Robot

--- Assets/_New Scripts_/Interactables/NavMeshJoystick.cs
+++ Assets/_New Scripts_/Interactables/NavMeshJoystick.cs
@@ -1,19 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NavMeshJoystick : SimpleHingeInteractable
 {
+    [SerializeField] NavMeshRobot robot;
+    [SerializeField] Transform rotationParentObject;
     [SerializeField] Transform trackedObject;
     [SerializeField] Transform trackingObject;
     protected override void ResetHinge()
     {
-        
+        if (robot != null)
+        {
+            robot.StopAgent();
+        }
     }
     protected override void Update()
     {
         base.Update();
-        trackingObject.position = new Vector3(trackedObject.position.x,
-        trackingObject.position.y, trackedObject.position.z);
+        if (isSelected)
+        {
+            MoveRobot();
+        }
+    }
+
+    private void MoveRobot()
+    {
+        if (robot != null)
+        {
+            trackingObject.position = new Vector3(trackedObject.position.x,
+                trackingObject.position.y, trackedObject.position.z);
+                
+            rotationParentObject.rotation = Quaternion.identity;
+            robot.MoveAgent(trackingObject.localPosition);
+        }
     }
 }
---
+++ Assets/_New Scripts_/Interactables/NavMeshRobot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events; 
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshRobot : MonoBehaviour
+{
+    public UnityEvent OnDestroyWallCube;
+    NavMeshAgent agent; 
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public void MoveAgent(Vector3 move)
+    {
+        agent.destination = agent.transform.position + move;
+    }
+    public void StopAgent()
+    {
+        agent.ResetPath();
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.transform.CompareTag("WallCube"))
+        {
+            Destroy(other.gameObject);
+            OnDestroyWallCube?.Invoke();
+        }
+    }
+}
